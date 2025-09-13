f5387772749193d8d3694808d8c71a244183d7d8
alexlewis9 <150391000+alexlewis9@users.noreply.github.com>
2025-09-13 18:25:07 -0400

    Auth0 (with errors currently)

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,22 @@
 import Map from "./components/Map";
 import { useRouter } from "next/navigation";
 
-export default function Home() {
+// eslint-disable-next-line @next/next/no-async-client-component
+export default async function Home() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const router = useRouter();
-  const isLoggedIn = typeof window !== "undefined" ? localStorage.getItem("isLoggedIn") === "true" : false;
+
+  const session = await auth0.getSession();
+
   return (
     <main className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 flex flex-col items-center justify-center p-8" style={{ position: "relative" }}>
       <button
-        onClick={() => router.push(isLoggedIn ? "/Account" : "/Login")}
+        onClick={() => router.push(isLoggedIn ? "/Account" : "/auth/login")}
         className="px-6 py-3 bg-blue-600 text-white text-lg font-semibold rounded-xl shadow hover:bg-blue-700 transition"
         style={{ position: "absolute", top: 32, right: 32 }}
       >
-        {isLoggedIn ? "Account" : "Log In"}
+        {session ? "Account" : "Log In"}
+        {/* TODO: add sign up button "/auth/login?screen_hint=signup" */}
       </button>
       <div className="w-full max-w-4xl bg-white rounded-2xl shadow-xl p-8 flex items-center justify-center">
         <Map />
