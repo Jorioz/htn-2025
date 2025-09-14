6bb0b6b1aa116c549d6c130c3f390340b786a181
Jorioz <jorgejacobrubio@gmail.com>
2025-09-13 20:47:17 -0400

    Additional Data

--- src/app/chargers.js
+++ src/app/chargers.js
@@ -1,28 +1,34 @@
 const chargers = [
-  {
-    id: 1,
-    name: "Charger A",
-    lat: 43.475,
-    lng: -80.542,
-    available: true,
-    rate: 0.25,
-  },
-  {
-    id: 2,
-    name: "Charger B",
-    lat: 43.470,
-    lng: -80.540,
-    available: false,
-    rate: 0.30,
-  },
-  {
-    id: 3,
-    name: "Charger C",
-    lat: 43.480,
-    lng: -80.545,
-    available: true,
-    rate: 0.28,
-  },
+    {
+        id: 1,
+        name: "Philip St. Level 2 Charger",
+        description: "268 Phillip St",
+        lat: 43.475,
+        lng: -80.542,
+        available: true,
+        rate: 0.25,
+        fee: 0.99,
+    },
+    {
+        id: 2,
+        name: "CHAdeMO Charger - UW Place",
+        description: "106 Seagram Dr",
+        lat: 43.47,
+        lng: -80.54,
+        available: false,
+        rate: 0.3,
+        fee: 1.3,
+    },
+    {
+        id: 3,
+        name: "UW Campus Easy Access Lvl 2",
+        description: "298 Longfellow Dr",
+        lat: 43.48,
+        lng: -80.545,
+        available: true,
+        rate: 0.28,
+        fee: 2,
+    },
 ];
 
 export default chargers;
